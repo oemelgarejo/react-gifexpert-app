33e2e86f322030233dd1869cb1652a8be8b4545c
Oscar Melgarejo <oemelgarejo3473@gmail.com>
2021-04-19 00:00:01 -0500

    first commit

---
+++ docs/precache-manifest.3ff8a44d650588bb6f1da1a5589f11ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892defaef70b4fcd549bb33040fd672a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b230788747327fac11",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b230788747327fac11",
+    "url": "/static/js/main.2e0efb31.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
